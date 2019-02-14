ae72ecb9a4b9fd342213d56c934491b38ec90324
Javier <bruzzesejavier@gmail.com>
2019-02-14 05:05:22 -0300

    v6

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,52 @@
 
 //Puerto:(env= enviroment)
 
-process.env.PORT=process.env.PORT||3000; //variable global.
+// process.env.PORT=process.env.PORT||3000; //variable global.
 
-//===================== ENTORNO ==================
-process.env.NODE_ENV= process.env.NODE_ENV || 'dev';
+// //===================== ENTORNO ==================
+// process.env.NODE_ENV= process.env.NODE_ENV || 'dev';
 
-//===================== BASE DE DATOS ==================
+// //===================== BASE DE DATOS ==================
 
+// let urlDB;
+
+// if(process.env.NODE_ENV==='dev'){
+// 	urlDB='mongodb://localhost:27017/cafe';
+// }else{
+
+// 	urlDB=process.env.MONGO_URI;
+// 	//'mongodb://javi047:sherlockip300@cafe-shard-00-00-nz7si.mongodb.net:27017,cafe-shard-00-01-nz7si.mongodb.net:27017,cafe-shard-00-02-nz7si.mongodb.net:27017/test?ssl=true&replicaSet=cafe-shard-0&authSource=admin&retryWrites=true'
+// };
+
+// process.env.URLDB=urlDB;
+
+
+
+//////////////////////////////////
+
+
+
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
 let urlDB;
 
-if(process.env.NODE_ENV==='dev'){
-	urlDB='mongodb://localhost:27017/cafe';
-}else{
-
-	urlDB=process.env.MONGO_URI;
-	//'mongodb://javi047:sherlockip300@cafe-shard-00-00-nz7si.mongodb.net:27017,cafe-shard-00-01-nz7si.mongodb.net:27017,cafe-shard-00-02-nz7si.mongodb.net:27017/test?ssl=true&replicaSet=cafe-shard-0&authSource=admin&retryWrites=true'
-};
-
-process.env.URLDB=urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
 
