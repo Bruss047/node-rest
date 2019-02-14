5fa13a9920e12d52f0b093dba49dc112bb62718f
Javier <bruzzesejavier@gmail.com>
2019-02-13 22:31:31 -0300

    v4

--- server/config/config.js
+++ server/config/config.js
@@ -7,13 +7,14 @@ process.env.PORT=process.env.PORT||3000; //variable global.
 process.env.NODE_ENV= process.env.NODE_ENV || 'dev';
 
 //===================== BASE DE DATOS ==================
+const MongoUri=process.env.MONGO_URI;
 let urlDB;
 
 if(process.env.NODE_ENV==='dev'){
 	urlDB='mongodb://localhost:27017/cafe';
 }else{
 
-	urlDB=process.env.MONGO_URI;
+	urlDB=MongoUri;
 };
 
 process.env.URLDB=urlDB;
