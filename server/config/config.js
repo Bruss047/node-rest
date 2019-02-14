4f4853983661932326442b3de46b748eb5278bd7
Javier <bruzzesejavier@gmail.com>
2019-02-13 22:37:26 -0300

    v5

--- server/config/config.js
+++ server/config/config.js
@@ -7,14 +7,15 @@ process.env.PORT=process.env.PORT||3000; //variable global.
 process.env.NODE_ENV= process.env.NODE_ENV || 'dev';
 
 //===================== BASE DE DATOS ==================
-const MongoUri=process.env.MONGO_URI;
+
 let urlDB;
 
 if(process.env.NODE_ENV==='dev'){
 	urlDB='mongodb://localhost:27017/cafe';
 }else{
 
-	urlDB=MongoUri;
+	urlDB=process.env.MONGO_URI;
+	//'mongodb://javi047:sherlockip300@cafe-shard-00-00-nz7si.mongodb.net:27017,cafe-shard-00-01-nz7si.mongodb.net:27017,cafe-shard-00-02-nz7si.mongodb.net:27017/test?ssl=true&replicaSet=cafe-shard-0&authSource=admin&retryWrites=true'
 };
 
 process.env.URLDB=urlDB;
